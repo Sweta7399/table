300ad6bb3265f313ca147b4097325da1c8e0a746
Sweta7399 <60713481+Sweta7399@users.noreply.github.com>
2020-11-08 11:52:50 +0530

    Add files via upload

---
+++ Table.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import "./table.css";
+
+function Table() {
+    return (
+    <div>
+        <div className="header1">
+        <span><span className="tme">8:00</span>
+        <span className="am">AM</span>
+        </span>
+        </div>
+
+        <div className="toptime"><span className="timefont">8:00</span></div>
+        <div className="Table">
+
+        <div className="table1">
+            <div className="block1">
+                <span className="toptext">2011 BMW 3 Series Sports Wagon</span>
+                <p><span className="belowtext">Bradford Flores</span><span className="mt">8:00 AM</span></p>
+            </div>
+            <div className="block2"><p className="leftside"><span className="bold">Contact:</span><span> 510-555-6988</span></p>
+            <p className="leftside2"><span className="bold">Services:</span><span> 2 Repairs | 1 Recall</span></p></div>
+            <div className="block4">
+                <p className="block4top">NOT ARRIVED</p>
+                <p className="block4below">Steve Robertson</p>
+            </div>
+            <div className="block5">
+                <span className="block4top">15.361 mi.</span>
+                <span className ="block4below2">Valet,Premium Cust...</span>
+            </div>
+    </div>
+    <div className="table2">
+            <div className="block12">
+                <span className="toptext">2013 BMW 6 Series</span>
+                <p><span className="belowtext1">Elisha Wolfe</span><span className="mt1">8:00 AM</span></p>
+            </div>
+            <div className="block22"><p className="leftside"><span className="bold">Contact:</span><span> 510-555-6988</span></p>
+            <p className="leftside2"><span className="bold">Services:</span><span> 2 Repairs | 1 Recall</span></p></div>
+            <div className="block42">
+                <p className="block4top">NOT ARRIVED</p>
+                <p className="block4below">Steve Robertson</p>
+            </div>
+            <div className="block52">
+                <span className="block4top">15.361 mi.</span>
+                <span className ="block4below2">Valet,Premium Cust...</span>
+            </div>
+    </div>
+    <div className="table3">
+            <div className="block12">
+                <span className="toptext">2008 BMW ActiveHybrid 5</span>
+                <p><span className="belowtext3">Jeaninnine Nolan</span><span className="mt2">8:00 AM</span></p>
+            </div>
+            <div className="block22"><p className="leftside"><span className="bold">Contact:</span><span> 510-555-6988</span></p>
+            <p className="leftside2"><span className="bold">Services:</span><span> 2 Repairs | 1 Recall</span></p></div>
+            <div className="block42">
+                <p className="block4top">NOT ARRIVED</p>
+                <p className="block4below">Steve Robertson</p>
+            </div>
+            <div className="block52">
+                <span className="block4top">15.361 mi.</span>
+                <span className ="block4below2">Valet,Premium Cust...</span>
+            </div>
+    </div>
+     </div>
+     <div className="toptime1"><span className="timefont1">8:45</span></div>
+     <div className="Table2">
+     <div className="table4">
+            <div className="block122">
+                <span className="toptext">2012 BMW 7 series</span>
+                <p><span className="belowtext1">Hunter Doyle</span><span className="mt1">8:00 AM</span></p>
+            </div>
+            <div className="block2"><p className="leftside"><span className="bold">Contact:</span><span> 510-555-6988</span></p>
+            <p className="leftside2"><span className="bold">Services:</span><span> 2 Repairs | 1 Recall</span></p></div>
+            <div className="block4">
+                <p className="block4top">NOT ARRIVED</p>
+                <p className="block4below">Steve Robertson</p>
+            </div>
+            <div className="block5">
+                <span className="block4top">15.361 mi.</span>
+                <span className ="block4below2">Valet,Premium Cust...</span>
+            </div>
+    </div>
+    </div>
+    <div className="Table3">
+    <div className ="notes">
+        <span className="notestext">Notes</span>
+    </div>
+    <div className="whitebox">
+        
+    </div>
+    </div>
+
+        </div> 
+
+        );
+      }
+    
+      export default Table;
